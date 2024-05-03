8988f4710dd6bb7155ad57ce7216c6c84a4d139b
Dooshima Hemen <hemendprincess@gmail.com>
2024-05-03 13:09:51 +0100

    Added 'change accent' to select + responsiveness

--- src/Components/TextToSpeech/TextToSpeech.jsx
+++ src/Components/TextToSpeech/TextToSpeech.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './TextToSpeech.css';
 import playbtn from '../Assets/play-button-arrowhead.png';
 
 const TextToSpeech = () => {
     let speech = new SpeechSynthesisUtterance();
-
+    let voices = [];
+    
     useEffect(() => {
         const handleClick = () => {
             speech.text = document.querySelector("textarea").value;
@@ -12,6 +13,25 @@ const TextToSpeech = () => {
         };
 
         document.querySelector("button").addEventListener("click", handleClick);
+
+        const voiceSelect = document.getElementById("select-lang"); // Moved declaration here
+
+        window.speechSynthesis.onvoiceschanged = () => {
+            voices = window.speechSynthesis.getVoices();
+            speech.voice = voices[0];
+            voiceSelect.innerHTML = ''; // Clear existing options
+            voices.forEach((voice, i) => {
+                const option = document.createElement('option');
+                option.text = voice.name;
+                option.value = i;
+                voiceSelect.appendChild(option);
+            });
+        };
+
+        voiceSelect.addEventListener("change", () => {
+            // When the value of the select dropdown changes, update the speech voice
+            speech.voice = voices[voiceSelect.value];
+        });        
 
         return () => {
             document.querySelector("button").removeEventListener("click", handleClick);
@@ -24,9 +44,9 @@ const TextToSpeech = () => {
                 <h1>Text-to-Speech <span>Converter</span></h1>
                 <textarea placeholder='Write anything here...'></textarea>
                 <div className="row">
-                    <select name='' id=''></select>
+                    <select name='select-lang' id='select-lang'></select>
                     <button className='btn'><img src={playbtn} alt="play icon" /> Listen</button>
-                </div>
+                </div> 
             </div>
         </div>
     );
